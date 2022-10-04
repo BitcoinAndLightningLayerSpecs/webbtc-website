54457b070600a21cab587e5c63b47807f78bffd4
René Aaron <rene@twentyuno.net>
2022-10-04 20:59:04 +0200

    fix: remove email

--- src/containers/webln/contact.tsx
+++ src/containers/webln/contact.tsx
@@ -7,7 +7,7 @@ export default function ContactWebLN() {
       description={
         "If your favorite app doesn't support WebLN yet, get in touch with the developer company and ask them to learn about how the protocol can help their users."
       }
-      email={"test@test.com"}
+      email={""}
     />
   );
 }
