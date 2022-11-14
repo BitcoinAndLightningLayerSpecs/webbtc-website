35c292a7b29790a9190aa842ab3c1f032d3c4f6e
Miguel Medeiros <miguel@miguelmedeiros.com.br>
2022-11-14 10:42:24 -0300

    fix: head tag move to app.js

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,37 +1,11 @@
 import Document, { Html, Head, Main, NextScript } from "next/document";
 import createEmotionServer from "@emotion/server/create-instance";
-import theme from "../src/theme";
 import createEmotionCache from "../src/createEmotionCache";
 
 export default class MyDocument extends Document {
-
   render() {
     return (
       <Html lang="en">
-        <Head>
-          {/* PWA primary color */}
-          <meta name="theme-color" content={theme.palette.primary.main} />
-          <link rel="shortcut icon" href="/static/favicon.ico" />
-          <link
-            rel="stylesheet"
-            href="https://fonts.googleapis.com/css?family=Inter:wght@100;200;300;400;500;600;700;800;900&display=swap"
-          />
-          <meta name="emotion-insertion-point" content="" />
-          {(this.props as any).emotionStyleTags}
-
-
-          <title>WebBTC - Combine the power of bitcoin with your website!</title>
-          <meta property="description" content="WebBTC - Combine the power of bitcoin with your website!" />
-          <meta property="og:title" content="WebBTC - Combine the power of bitcoin with your website!" />
-          <meta property="og:type" content="website" />
-          <meta property="og:url" content={process.env.NEXT_PUBLIC_DOMAIN}/>
-          <meta property="og:image" content={`${process.env.NEXT_PUBLIC_DOMAIN}/images/social/preview.png`} />
-          
-          <meta name="twitter:title" content="WebBTC - Combine the power of bitcoin with your website!" />
-          <meta name="twitter:description" content="WebBTC is a set of specifications for web apps and client providers to facilitate communication with Bitcoin & Lightning wallets. It provides a programmatic, permissioned interface for letting web apps send or receive payments and much more."/>
-          <meta name="twitter:image" content={`${process.env.NEXT_PUBLIC_DOMAIN}/images/social/preview.png`} />
-          <meta name="twitter:card" content="summary_large_image"/>
-        </Head>
         <body>
           <Main />
           <NextScript />
